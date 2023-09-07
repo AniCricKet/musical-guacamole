85c6007a0ebbf015e01b8e50cdcdf96289bd3453
Aniket Chakradeo <aniket@chakradeo.net>
2023-09-07 11:51:05 -0700

    console game working :)

--- _notebooks/GuessTheNumberGame.java
+++ _notebooks/GuessTheNumberGame.java
@@ -5,20 +5,20 @@ public class GuessTheNumberGame {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         Random random = new Random();
-        
-        int minRange = 1;    // Minimum number in the range
-        int maxRange = 100;  // Maximum number in the range
+
+        int minRange = 1;
+        int maxRange = 100;
         int secretNumber = random.nextInt(maxRange - minRange + 1) + minRange;
         int attempts = 0;
-        
+
         System.out.println("Welcome to Guess the Number Game!");
         System.out.println("I have selected a number between " + minRange + " and " + maxRange + ". Try to guess it!");
-        
+
         while (true) {
             System.out.print("Enter your guess: ");
             int userGuess = scanner.nextInt();
             attempts++;
-            
+
             if (userGuess < minRange || userGuess > maxRange) {
                 System.out.println("Please enter a number between " + minRange + " and " + maxRange + ".");
             } else if (userGuess < secretNumber) {
@@ -30,6 +30,7 @@ public class GuessTheNumberGame {
                 break;
             }
         }
-        
+
         scanner.close();
     }
+}
